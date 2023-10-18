e6a166696a36f8baabc7b704895d141a8fea6cfb
kuworking <kuworking@gmail.com>
2023-10-18 21:18:10 +0200

    eliminate spare fonts

--- uno.config.ts
+++ uno.config.ts
@@ -5,11 +5,13 @@ export default defineConfig({
   shortcuts: {
     'responsive-divs': 'max-w-fit px-[10px] mx-auto sm:max-w-[460px] md:max-w-[660px] lg:max-w-[860px]',
     'responsive-divs-narrow': 'max-w-fit px-[10px] mx-auto sm:max-w-[460px] md:max-w-[660px] lg:max-w-[700px]',
-    'is-link': 'text-[--normal] hover:text-[--remark] dark:text-[--dark-normal] dark:hover:text-[--dark-remark] font-light no-underline',
-    'is-special-link': 'text-[--remark] dark:text-[--dark-remark] hover:text-[#424242] dark:hover:text-[#AE7373] font-bold',
+    'is-link':
+      'text-[--normal] hover:text-[--remark] dark:text-[--dark-normal] dark:hover:text-[--dark-remark] font-light no-underline',
+    'is-special-link':
+      'text-[--remark] dark:text-[--dark-remark] hover:text-[#424242] dark:hover:text-[#AE7373] font-bold',
     'is-em': 'bg-[--remark] dark:bg-[--dark-remark] text-[--dark-normal] dark:text-[--normal]',
     'is-code': '',
-    'trans-150': 'transition duration-150 ease-in-out'
+    'trans-150': 'transition duration-150 ease-in-out',
   },
   theme: {
     breakpoints: {
@@ -31,32 +33,12 @@ export default defineConfig({
       provider: 'google', // default provider
       fonts: {
         // these will extend the default theme
-        sans: 'Roboto',
         mono: ['Fira Code', 'Fira Mono:400,700'],
         // custom ones
-        lobster: 'Lobster',
         roboto: [
           {
             name: 'Roboto',
             weights: ['300', '700', '900'],
-          },
-        ],
-        bebas: 'Bebas Neue',
-        urbanist: [
-          {
-            name: 'Urbanist',
-            weights: ['400', '700', '900'],
-          },
-        ],
-        lato: [
-          {
-            name: 'Lato',
-            weights: ['400', '700'],
-            italic: true,
-          },
-          {
-            name: 'sans-serif',
-            provider: 'none',
           },
         ],
       },
